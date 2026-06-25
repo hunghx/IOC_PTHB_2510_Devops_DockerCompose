2a49af8c7eb87a339dc69094b0ab989f4d3adaae
hunghx <baby10051999@gmail.com>
2026-06-25 10:27:19 +0700

    git update

--- course-service/src/main/java/com/example/courseservice/client/UserClient.java
+++ course-service/src/main/java/com/example/courseservice/client/UserClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-service", url = "${user.service.url:http://localhost:8081}")
+@FeignClient(name = "auth-service", url = "${user.service.url:http://auth-service:8081}")
 public interface UserClient {
 
     @GetMapping("/users/{id}")
